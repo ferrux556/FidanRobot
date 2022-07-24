412dfd8ecf4fbc5cd77410480864638d2634dc89
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-07-24 11:50:16 +0400

    update nezrintxt.py

--- SaitamaRobot/modules/nezrintxt.py
+++ SaitamaRobot/modules/nezrintxt.py
@@ -7,12 +7,12 @@ from SaitamaRobot import pbot
 from AykhanPro.komekci import random_line
 
 
-@pbot.on_message(filters.command("meslehet") & ~filters.edited)
+@pbot.on_message(filters.private & filters.incoming & filters.command(('meslehet')))
 async def meslehet(_, message):
     await message.reply_text((await random_line('AykhanPro/txt/meslehet.txt')))
 
 
-@pbot.on_message(filters.command("anekdod") & ~filters.edited)
+@pbot.on_message(filters.private & filters.incoming & filters.command(('anekdod')))
 async def anekdod(_, message):
     await message.reply_text((await random_line('AykhanPro/txt/anekdod.txt')))
 
